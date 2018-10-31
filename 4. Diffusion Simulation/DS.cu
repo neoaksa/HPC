ca78b59808248c31024f00f7c61f034c01efbd95
Jie Tao <34007729+neoaksa@users.noreply.github.com>
2018-10-31 10:38:48 -0400

    Add files via upload

--- 4. Diffusion Simulation/DS.cu
+++ 4. Diffusion Simulation/DS.cu
@@ -2,6 +2,7 @@
  * nvcc -arch=sm_30 DS.cu -run
  * To compile with: nvcc -O2 -o DS DS.cu
  * reference:Inter-Block GPU Communication via Fast Barrier Synchronization
+ * nvcc -arch compute_30 DS.cu -o DS
  * */
 
 #include <stdio.h>
@@ -11,21 +12,20 @@
 
 // set a 3D volume
 //define the data set size (cubic volume)
-#define DATAXSIZE 512
-#define DATAYSIZE 512
-#define DATAZSIZE 512
+#define DATAXSIZE 64
+#define DATAYSIZE 64
+#define DATAZSIZE 64
 //block size = 8*8*8 = 512
 #define BLKXSIZE 8
 #define BLKYSIZE 8
 #define BLKZSIZE 8
 //time iteration
-#define t 10
+#define t 1
 
-__device__ int barrier = DATAXSIZE/BLKXSIZE;
 
 // device function to set the 3D volume
 __global__ void diffusion(unsigned int *init_pos, float (*output_array)[DATAYSIZE][DATAXSIZE],
-                          float (*shadow_array)[DATAYSIZE][DATAXSIZE], float con_begin)
+                          float (*shadow_array)[DATAYSIZE][DATAXSIZE], float con_begin, int *barrier, int *barrierOut)
 {   
 //     // get grid, only works on new GPU
 //     grid_group g = this_grid();
@@ -33,6 +33,17 @@ __global__ void diffusion(unsigned int *init_pos, float (*output_array)[DATAYSIZ
     unsigned idx = blockIdx.x*blockDim.x + threadIdx.x;
     unsigned idy = blockIdx.y*blockDim.y + threadIdx.y;
     unsigned idz = blockIdx.z*blockDim.z + threadIdx.z;
+    // unique block id and tid
+    unsigned bid = blockIdx.x 
+                + blockIdx.y*gridDim.x 
+                + blockIdx.z*gridDim.x*gridDim.y;
+//     unsigned tid = idx 
+//                 + (idy * gridDim.x)
+//                 + (idz * gridDim.x * gridDim.y);
+    unsigned tid_in_block = threadIdx.x
+                            + threadIdx.y * blockDim.x
+                            + threadIdx.z * blockDim.y * blockDim.x;
+    unsigned blockNum = gridDim.x*gridDim.y*gridDim.z;
     // inital concetration
     if(idx>=init_pos[0] && idx<=init_pos[1] 
         && idy>=init_pos[2] && idy<=init_pos[3] 
@@ -52,24 +63,42 @@ __global__ void diffusion(unsigned int *init_pos, float (*output_array)[DATAYSIZ
         }
 //         //sync within grid
 //         g.sync();
-        //sync between blocks
          __syncthreads();
-        if ( threadIdx.x == 0 && threadIdx.y==0 && threadIdx.z ==0 )
-            atomicSub( &barrier , 1 );
+        //sync between blocks
+        // the first thread in each block takes charge of update barrier
+        if ( tid_in_block == 0 )
+            barrier[bid]=1;
         
-        /* Now wait for the barrier to be zero. */
-        if ( threadIdx.x == 0 && threadIdx.y==0 && threadIdx.z ==0  ){
-            while ( atomicCAS( &barrier , 0 , 0 ) != 0 );
+        // the first N thread in block 1 response to check barrier? Problem here when block num > block size
+        if(bid==1){
+           if(tid_in_block < blockNum){
+                while(barrier[tid_in_block]!=1);//{printf("%d-%d \n",bid,tid_in_block);};
+            }
+            __syncthreads();
+            if(tid_in_block < blockNum){
+                barrierOut[tid_in_block]=1;
+            }  
         }
-//         // refresh shadow array
-//         shadow_array[idz][idy][idx] = output_array[idz][idy][idx];
-//         /* Make sure everybody has waited for the barrier. */
+        //all other threads check barrierOut
+        if(tid_in_block==0){
+            while(barrierOut[bid]!=1){printf("%d-%d \n",tid_in_block,bid);};
+        }
         __syncthreads();
-//          if ( threadIdx.x == 0 && threadIdx.y==0 && threadIdx.z ==0  && atomicCAS( &barrier , 0 , 0 ) == 0 ){
-//             atomicAdd(&barrier, DATAXSIZE/BLKXSIZE);
-//          }
-//         /* Make sure everybody has waited for the barrier. */
-//         __syncthreads();
+        // updae shadow and reset barrier 
+        shadow_array[idz][idy][idx] = output_array[idz][idy][idx];
+        if(bid==1){
+            if(tid_in_block < blockNum){
+                barrier[tid_in_block]=0;
+                barrierOut[tid_in_block]=0;
+            }
+            __syncthreads();
+        }
+        else{
+            if(tid_in_block==0){    
+                while(barrierOut[bid]!=0);
+            }
+            __syncthreads();
+        }
     }
 }
 
@@ -97,6 +126,8 @@ int main(int argc, char *argv[])
     nRarray *output_c; // storage for result stored on host
     nRarray *output_d;  // storage for result computed on device
     nRarray *shadow_d; // shadow array for saving temp value
+    int *barrier; // array for sync blocks
+    int *barrierOut; // array for sync blocks(stop)
     // allocate storage for receiving output
     if ((output_c = (nRarray *)malloc((nx*ny*nz)*sizeof(float))) == 0) {
 		fprintf(stderr,"malloc1 Fail \n"); return 1;
@@ -107,14 +138,24 @@ int main(int argc, char *argv[])
 		fprintf(stderr, ("Failed to allocate device buffer-inital_d"));
 		exit(1);
 	}
-    result = cudaMalloc((void **) &output_d, (nx*ny*nz)*sizeof(float));
+    result = cudaMalloc((void **) &output_d, (int)(nx*ny*nz)*sizeof(float));
     if (result != cudaSuccess) {
 		fprintf(stderr, ("Failed to allocate device buffer-output_d"));
 		exit(1);
 	}
-    result = cudaMalloc((void **) &shadow_d, (nx*ny*nz)*sizeof(float));
+    result = cudaMalloc((void **) &shadow_d, (int)(nx*ny*nz)*sizeof(float));
     if (result != cudaSuccess) {
 		fprintf(stderr, ("Failed to allocate device buffer- shadow_d"));
+		exit(1);
+	}
+	result = cudaMalloc((void **) &barrier, ((int)(DATAXSIZE*DATAYSIZE*DATAZSIZE)/(int)(BLKXSIZE*BLKYSIZE*BLKZSIZE))*sizeof(int));
+    if (result != cudaSuccess) {
+		fprintf(stderr, ("Failed to allocate device buffer-barrier"));
+		exit(1);
+	}
+	result = cudaMalloc((void **) &barrierOut, ((int)(DATAXSIZE*DATAYSIZE*DATAZSIZE)/(int)(BLKXSIZE*BLKYSIZE*BLKZSIZE))*sizeof(int));
+    if (result != cudaSuccess) {
+		fprintf(stderr, ("Failed to allocate device buffer-barrierOut"));
 		exit(1);
 	}
 	// copy host to device
@@ -127,15 +168,16 @@ int main(int argc, char *argv[])
     const dim3 blockSize(BLKXSIZE, BLKYSIZE, BLKZSIZE);
     const dim3 gridSize((DATAXSIZE/BLKXSIZE), (DATAYSIZE/BLKYSIZE), (DATAZSIZE/BLKZSIZE));
     
-    diffusion<<<gridSize,blockSize>>>(inital_d,output_d,shadow_d,con_begin);
+    diffusion<<<gridSize,blockSize>>>(inital_d,output_d,shadow_d,con_begin,barrier,barrierOut);
     // copy output data back to host
-    result = cudaMemcpy(output_c, output_d, ((nx*ny*nz)*sizeof(float)), cudaMemcpyDeviceToHost);
+    result = cudaMemcpy(output_c, output_d, ((int)(nx*ny*nz)*sizeof(float)), cudaMemcpyDeviceToHost);
     if (result != cudaSuccess) {
 		fprintf(stderr, ("cudaMemcpy dev (block)->host failed."));
 		exit(1);
 	}
-	
-	//TEST
+	cudaDeviceSynchronize();
+//     cudaDeviceReset();
+// 	//TEST
     for (unsigned i=0; i<nz; i++)
       for (unsigned j=0; j<ny; j++)
         for (unsigned k=0; k<nx; k++){
@@ -147,17 +189,27 @@ int main(int argc, char *argv[])
     free(output_c);
     result = cudaFree(shadow_d);
     if (result != cudaSuccess) {
-		fprintf(stderr, ("Failed to allocate device buffer"));
+		fprintf(stderr, ("Failed to Free device buffer - shadow_d"));
 		exit(1);
 	}
     result = cudaFree(output_d);
     if (result != cudaSuccess) {
-		fprintf(stderr, ("Failed to allocate device buffer"));
+		fprintf(stderr, ("Failed to Free device buffer - output_d"));
 		exit(1);
 	}
 	result = cudaFree(inital_d);
     if (result != cudaSuccess) {
-		fprintf(stderr, ("Failed to allocate device buffer"));
+		fprintf(stderr, ("Failed to Free device buffer - inital_d"));
+		exit(1);
+	}
+		result = cudaFree(barrier);
+    if (result != cudaSuccess) {
+		fprintf(stderr, ("Failed to Free device buffer - barrier"));
+		exit(1);
+	}
+		result = cudaFree(barrierOut);
+    if (result != cudaSuccess) {
+		fprintf(stderr, ("Failed to Free device buffer - barrierOut"));
 		exit(1);
 	}
     return 0;
